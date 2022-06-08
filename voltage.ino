f7ee858df69feaf8a50b9bbf60f16e32f4298d9f
Ferry Ariyanto <64236762+205322111846@users.noreply.github.com>
2022-06-08 20:10:48 +0700

    Update voltage.ino

--- voltage.ino
+++ voltage.ino
@@ -1,6 +1,25 @@
 #include <Arduino.h>
 #include <SensorModbusMaster.h>
+#include <FirebaseESP32.h>
+#include <WiFiMulti.h>
 
+// define available SSID & PASS
+#define SSID1 "R13"
+#define PASS1 "enerpe31"
+#define SSID2 "add"
+#define PASS2 "11223344"
+#define SSID3 "Ular Sanca"
+#define PASS3 "12093487"
+#define SSID4 "AncolKentang"
+#define PASS4 "ppqooqiiq123"
+
+#define Firebase_Host "monitoring-daya-ppns-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define Firebase_Auth "QpcYZuWZUjfsoReNe8zU31BZOdg26fSEPQObsbns"
+FirebaseData fbdo;
+FirebaseJson fjson;
+//initialize variabel and etc
+WiFiMulti AP;
+String path = "/ruangan2";
 //Modbus
 byte modbusAddress = 0x01;
 long modbusBaudRate = 9600;
@@ -23,10 +42,6 @@ modbusMaster modbus;
 //#define Firebase_Host "monitoring-daya-ppns-default-rtdb.asia-southeast1.firebasedatabase.app"
 //#define Firebase_Auth "QpcYZuWZUjfsoReNe8zU31BZOdg26fSEPQObsbns"
 
-//Delay
-unsigned long delayStart = 0;
-bool delayRunning = false;
-
 void setup() {
   
   if(DEREPin >= 0){
@@ -46,23 +61,76 @@ void setup() {
   #endif
 
   modbus.begin(modbusAddress, modbusSerial, DEREPin);
+  setupWiFi();
+  while(AP.run() != WL_CONNECTED) {
+    Serial.print('.');
+    
+    delay(500);
+  }
+  Serial.println();
+  Firebase.begin(Firebase_Host, Firebase_Auth);
+  Serial.println("Firebase init");
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  int16_t voltage = 0;
-  int32_t power = 0;
-  int16_t arus = 0;
+  int16_t tegR = 0;
+  int16_t tegS = 0;
+  int16_t tegT = 0;
+  int16_t arusR = 0;
+  int16_t arusS= 0;
+  int16_t arusT = 0;
+  int16_t daya = 0;
+  int32_t kwh = 0;
       
-  voltage = modbus.uint16FromRegister(0x03, 0x00, bigEndian); //tegangan
-  power = modbus.uint16FromRegister(0x03, 0x1D, bigEndian); //daya
-  arus = modbus.uint16FromRegister(0x03, 0x03, bigEndian); //arus
-      
-  Serial.print("Volt ");
-  Serial.println((double)voltage*0.1,1);
-  Serial.print("Power ");
-  Serial.println((double)power*0.01,2);
-  Serial.print("Current ");
-  Serial.println((double)arus*0.001,3);
+  tegR = modbus.uint16FromRegister(0x03, 0x00, bigEndian); //tegangan R
+  tegS = modbus.uint16FromRegister(0x03, 0x01, bigEndian); //tegangan S
+  tegT = modbus.uint16FromRegister(0x03, 0x02, bigEndian); //tegangan T
+  arusR = modbus.uint16FromRegister(0x03, 0x03, bigEndian); //arus R
+  arusS = modbus.uint16FromRegister(0x03, 0x04, bigEndian); //arus S
+  arusT = modbus.uint16FromRegister(0x03, 0x05, bigEndian); //arus T
+  daya = modbus.uint16FromRegister(0x03, 0x07, bigEndian); //daya
+  kwh = modbus.uint32FromRegister(0x03, 0x1D, bigEndian); //kwh
+  
+  fjson.set("Timestamp/.sv", "timestamp");
+  fjson.set("daya", daya);
+  fjson.set("kwh", kwh);
+  fjson.set("tegR", tegR);
+  fjson.set("tegS", tegS);
+  fjson.set("tegT", tegT);
+  fjson.set("arusR", arusR);
+  fjson.set("arusS", arusS);
+  fjson.set("arusT", arusT);
+  if(!Firebase.pushJSON(fbdo, path, fjson)) {
+    Serial.printf("Error: %s\n", fbdo.errorReason().c_str());
+  } else {
+    Serial.println("Sukses");
+  }
+  
+  Serial.print("Volt R-");
+  Serial.print((double)tegR*0.1,1);
+  Serial.print(" S-");
+  Serial.print((double)tegS*0.1,1);
+  Serial.print(" T-");
+  Serial.println((double)tegT*0.1,1);
+  Serial.print("Current R-");
+  Serial.print((double)arusR*0.001,3);
+  Serial.print(" S-");
+  Serial.print((double)arusS*0.001,3);
+  Serial.print(" T-");
+  Serial.println((double)arusT*0.001,3);
+  Serial.print("Daya ");
+  Serial.println((double)daya,0);
+  Serial.print("kWh ");
+  Serial.println((double)kwh*0.01,2);
 
-  delay(10UL * 60UL * 1000UL);
+  delay(10UL * 60UL * 1000UL); //delay 10 menit
+//  delay(100);
+}
+
+void setupWiFi(){
+  AP.addAP(SSID1, PASS1);
+  AP.addAP(SSID2, PASS2);
+  AP.addAP(SSID3, PASS3);
+  AP.addAP(SSID4, PASS4);
 }
